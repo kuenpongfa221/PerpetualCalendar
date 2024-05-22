a3aecd999d4bde3c29885c0c7408add1bd4889fb
Shao Bin <lifeprocess91@gmail.com>
2024-05-22 14:06:14 +0800

    成功修改出 三個代辦事項都可以使用了!! jq她媽終於成功改出來了!!!!

--- jq-text-to-input-test.js
+++ jq-text-to-input-test.js
@@ -12,13 +12,17 @@ function clickDiv(e) {
   // console.log(e);
   let input = e.srcElement;
   let inputValue = input.innerHTML;
+  let inputClassName = input.className;
   let jqInput = $(input);
   console.log(jqInput);
   jqInput.replaceWith(
     `<input class=${input.className} type='text' value='${inputValue}' onkeydown='clickInput(event)'>`
   );
 
-  jqInput = $(".todolist-item-1");
+  jqInput = $(`.${inputClassName}`);
+  // console.log(typeof inputClassName);
+  // console.log(inputClassName);
+  // console.log(jqInput);
   jqInput.focus();
   jqInput.val("");
   jqInput.val(inputValue);
@@ -51,10 +55,10 @@ function clickInput(e) {
 }
 
 $(document).ready(function () {
+
   $(".todolist-item-1").on("click", () => {
     let input = $(".todolist-item-1");
     $(".todolist-item-1").replaceWith(
-      // `<input class='todolist-item-1' type='text' value='${input.text()}' onkeydown='clickInput(event, ${input}, ${class_name})'   />`
       `<input class='todolist-item-1' type='text' value='${input.text()}' onkeypress='clickInput(event)'   />`
     );
 
@@ -64,8 +68,6 @@ $(document).ready(function () {
 
     $(".todolist-item-1").on("blur", () => {
       input = $(".todolist-item-1");
-      // console.log(e);
-      // console.log(inputValue);
       $(".todolist-item-1").replaceWith(
         `<div class='todolist-item-1' onclick='clickDiv(event)'>${input.val()}</div>`
       );
@@ -75,20 +77,50 @@ $(document).ready(function () {
   });
 
 
-  // $(".todolist-item-2").on("click", () => {
-  //   let input = $(".todolist-item-2");
-  //   $(".todolist-item-2").replaceWith(
-  //     // `<input class='todolist-item-1' type='text' value='${input.text()}' onkeydown='clickInput(event, ${input}, ${class_name})'   />`
-  //     `<input class='todolist-item-2' type='text' value='${input.text()}' onkeydown='clickInput(event)' onblur='blurInput(event)'   />`
-  //   );
+  $(".todolist-item-2").on("click", () => {
+    let inputBefore = $(".todolist-item-2");
+    inputBefore.replaceWith(
+      `<input class='todolist-item-2' type='text' value='${inputBefore.text()}' onkeypress='clickInput(event)'   />`
+    );
 
-  //   $(".todolist-item-2").focus();
-  //   $(".todolist-item-2").val("");
-  //   $(".todolist-item-2").val(input.text());
+    console.log(inputBefore);
+    inputAfter = $(".todolist-item-2");
+    inputAfter.focus();
+    inputAfter.val("");
+    inputAfter.val(inputBefore.text());
 
-  //   //html 的event一定要打event，不能打e
-  //   //   $(".text").attr("onkeydown", `myFunction(event)`);
-  // });
+    inputAfter.on("blur", () => {
+      inputAfter = $(".todolist-item-2");
+      inputAfter.replaceWith(
+        `<div class='todolist-item-2' onclick='clickDiv(event)'>${inputAfter.val()}</div>`
+      );
+    });
+    //html 的event一定要打event，不能打e
+    //   $(".text").attr("onkeydown", `myFunction(event)`);
+  });
+
+
+  $(".todolist-item-3").on("click", () => {
+    let inputBefore = $(".todolist-item-3");
+    inputBefore.replaceWith(
+      `<input class='todolist-item-3' type='text' value='${inputBefore.text()}' onkeypress='clickInput(event)'   />`
+    );
+
+    console.log(inputBefore);
+    inputAfter = $(".todolist-item-3");
+    inputAfter.focus();
+    inputAfter.val("");
+    inputAfter.val(inputBefore.text());
+
+    inputAfter.on("blur", () => {
+      inputAfter = $(".todolist-item-3");
+      inputAfter.replaceWith(
+        `<div class='todolist-item-3' onclick='clickDiv(event)'>${inputAfter.val()}</div>`
+      );
+    });
+    //html 的event一定要打event，不能打e
+    //   $(".text").attr("onkeydown", `myFunction(event)`);
+  });
 
 
 });
